ba54991e5b8e4444ae0b3ee91cd81f33a687ddce
kalyaniisampelliwar <kalyaniis.0908@gmail.com>
2020-07-10 20:01:29 +0530

    testing commit

---
+++ src/practice_package/Class_Practice.java
@@ -0,0 +1,15 @@
+package practice_package;
+
+import org.testng.annotations.Test;
+
+public class Class_Practice {
+	
+	@Test
+	public void test1(){
+		System.out.println("Hello");
+		
+	}
+	
+	
+
+}
